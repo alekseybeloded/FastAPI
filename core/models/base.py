38b63434aad522187c51859d47f8431a48842fe2
alekseybeloded <belod-aleksej@yandex.ru>
2024-11-29 23:52:28 +0300

    feat: init database

--- core/models/base.py
+++ core/models/base.py
@@ -1,7 +1,14 @@
 import datetime
+import os
 
-from sqlalchemy import DateTime
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import DateTime, create_engine
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    scoped_session,
+    sessionmaker,
+)
 
 
 class Base(DeclarativeBase):
@@ -10,3 +17,8 @@ class Base(DeclarativeBase):
     updated_at: Mapped[datetime.datetime] = mapped_column(DateTime())
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
+
+db_url = os.environ['DB_URL']
+engine = create_engine("sqlite://", echo=True)
+db_session = scoped_session(sessionmaker(bind=engine))
