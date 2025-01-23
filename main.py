8bdab7f11b81e19751ad4eaca779655c08b95536
alekseybeloded <belod-aleksej@yandex.ru>
2025-01-23 23:38:11 +0300

    chore: add dispose method to shutdown

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
-from teams.views import router as teams_router
+from api import router as api_router
+from core.models import db_helper
 
-app = FastAPI()
-app.include_router(teams_router)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncIterator:
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+
+main_app = FastAPI(
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+)
+main_app.include_router(api_router)
